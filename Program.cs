6a9cd8af9e1b472220992bb355d34e276ddcc426
DavidD <david.dognin@proton.me>
2022-06-03 20:54:34 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LexerParser_CSharp {
+  public class HelloWorld {
+      public static void Main() 
+      {
+          Lexer lex = new Lexer();
+          Console.WriteLine("--- Lexer Parser ---");
+          Console.WriteLine("Veuillez entrer un calcul.");
+          String result = Console.ReadLine();
+          lex.parseText(result);
+          Console.WriteLine(lex.ToString());
+      }
+  }
+}
